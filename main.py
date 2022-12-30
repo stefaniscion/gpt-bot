8f831f1acd23a4b2184564a2cefd579cd6d2e138
Stefano Di Pasquale <stefaniscion@gmail.com>
2022-12-31 00:02:10 +0100

    added Procfile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import telebot
 import openai
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -12,4 +11,22 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    
     return {"message": "Hello World"}
+
+def parse_message(message):
+    print("message-->",message)
+    chat_id = message['message']['chat']['id']
+    txt = message['message']['text']
+    print("chat_id-->", chat_id)
+    print("txt-->", txt)
+    return chat_id,txt
+
+def tel_send_message(chat_id, text):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    r = requests.post(url,json=payload)
+    return r
